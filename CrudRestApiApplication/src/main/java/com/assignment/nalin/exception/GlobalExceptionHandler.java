1d0c53a70a17fb292898f55d3b76d7cfebb3863f
Nalinshankar <45656324+Nalinshankar@users.noreply.github.com>
2021-04-08 18:27:36 +0530

    Add files via upload

--- CrudRestApiApplication/src/main/java/com/assignment/nalin/exception/GlobalExceptionHandler.java
+++ CrudRestApiApplication/src/main/java/com/assignment/nalin/exception/GlobalExceptionHandler.java
@@ -1,14 +1,23 @@
 package com.assignment.nalin.exception;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
@@ -21,4 +30,32 @@ public class GlobalExceptionHandler {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+    
+    @Bean
+    public ErrorAttributes errorAttributes() {
+      // Hide exception field in the return object
+      return new DefaultErrorAttributes() {
+        @Override
+        public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+          Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+          errorAttributes.remove("exception");
+          return errorAttributes;
+        }
+      };
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public void handleCustomException(HttpServletResponse res, CustomException ex) throws IOException {
+      res.sendError(ex.getHttpStatus().value(), ex.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public void handleAccessDeniedException(HttpServletResponse res) throws IOException {
+      res.sendError(HttpStatus.FORBIDDEN.value(), "Access denied");
+    }
+
+//    @ExceptionHandler(Exception.class)
+//    public void handleException(HttpServletResponse res) throws IOException {
+//      res.sendError(HttpStatus.BAD_REQUEST.value(), "Something went wrong");
+//    }
 }
